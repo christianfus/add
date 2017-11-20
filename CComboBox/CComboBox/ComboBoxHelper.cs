9f5bcdd068af664a9d80aafe4679b30d2f476e3a
christianfus <christianfus@gmail.com>
2017-11-20 13:45:37 +0100

    Añadido CComboBox

---
+++ CComboBox/CComboBox/ComboBoxHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Gtk;
+using System.Data;
+using MySql.Data;
+using Serpis.Ad;
+
+namespace Serpis.Ad
+{
+    public class ComboBoxHelper
+    {
+        public const string ComboNullLabel = "<sin asignar>";
+        public static void Fill(ComboBox comboBox, string selectSql, object id){
+            CellRendererText CellRendererText = new CellRendererText();
+            comboBox.PackStart(CellRendererText, false);
+            comboBox.AddAttribute(CellRendererText, "text", 1);
+
+            IDbCommand dbCommand = App.Instance.Connection.CreateCommand();
+            dbCommand.CommandText = selectSql;
+            IDataReader dataReader = dbCommand.ExecuteReader();
+
+            ListStore listStore = new ListStore(typeof(string), typeof(string));
+            TreeIter initialTreeIter = listStore.AppendValues("0", ComboNullLabel);
+            while(dataReader.Read()) {
+                TreeIter treeIter = listStore.AppendValues(dataReader[0].ToString(), dataReader[1].ToString());
+                if (id.Equals(dataReader[0]))
+                    initialTreeIter = treeIter;
+
+            }
+        }
+    }
+}
---
+++ CComboBox/CComboBox/MainWindow.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+
+        CellRendererText idCellRendererText = new CellRendererText();
+        comboBox.PackStart(idCellRendererText, false);
+        comboBox.AddAttribute(idCellRendererText, "text", 0);
+        CellRendererText labelCellRenderText = new CellRendererText();
+        comboBox.PackStart(labelCellRenderText, false);
+        comboBox.AddAttribute(labelCellRenderText, "text", 1);
+
+
+        ListStore listStore = new ListStore(typeof(string), typeof(string));
+        comboBox.Model = listStore;
+        TreeIter treeIter = listStore.AppendValues("0", "<sin asignar>");
+        listStore.AppendValues("1", "primero");
+		listStore.AppendValues("2", "segundo");
+
+        comboBox.SetActiveIter(treeIter);
+
+	}
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
